31cb69730b79d079992c42c7e518db658d51b7b5
blamster19 <blamster19@gmail.com>
2023-07-25 22:47:03 +0200

    refactor: split modules into separate files

---
+++ src/interface/git_integration.rs
@@ -0,0 +1,56 @@
+use git2::Repository;
+use std::fs;
+use std::io::Write;
+use std::path::PathBuf;
+
+pub(in crate::interface) fn commit(repo: &Repository, message: &str) {
+	let mut index = repo.index().expect("Failed to get Index file");
+	index
+		.add_all(["*"].iter(), git2::IndexAddOption::DEFAULT, None)
+		.expect("Failed to commit");
+	index.write().expect("Failed to commit");
+	let signature =
+		git2::Signature::now("cru", "cru@xyz.net").expect("Failed to create a signature");
+	let oid = index.write_tree().expect("Couldn't write tree");
+	let tree = repo.find_tree(oid).expect("Couldn't find tree");
+	match repo.head() {
+		Ok(head) => {
+			let parent = head
+				.resolve()
+				.unwrap()
+				.peel_to_commit()
+				.expect("Couldn't find last commit");
+			repo.commit(
+				Some("HEAD"),
+				&signature,
+				&signature,
+				message,
+				&tree,
+				&[&parent],
+			)
+			.expect("Failed to commit");
+		}
+		Err(_e) => {
+			repo.commit(Some("HEAD"), &signature, &signature, message, &tree, &[])
+				.expect("Failed to commit");
+		}
+	};
+}
+
+pub(in crate::interface) fn create_new_repo(path: &PathBuf) {
+	fs::create_dir_all(path.clone())
+		.expect("Failed to create working directory. Please check the permissions.");
+	let repo = match Repository::init(path) {
+		Ok(repo) => repo,
+		Err(e) => panic!("Failed to init a git repository: {}", e),
+	};
+	create_config(&path);
+	std::fs::create_dir_all(path.join("records.d")).expect("Failed to create directory");
+	commit(&repo, "Initial commit");
+}
+
+fn create_config(path: &PathBuf) {
+	let mut conffile = fs::File::create(path.join("conf")).expect("Failed to create config file");
+	write!(&mut conffile, "[GIT SETTINGS]\nremote = no").expect("Failed to write to config file");
+	fs::File::create(path.join("records")).expect("Failed to create records file");
+}
